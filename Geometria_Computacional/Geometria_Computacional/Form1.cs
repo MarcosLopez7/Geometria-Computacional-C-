50ac09cefcfee2f52c1558c08fbcc8410e757b38
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2014-10-29 14:52:23 -0600

    Creación de la clase geometria computacional
    s

--- Geometria_Computacional/Geometria_Computacional/Form1.cs
+++ Geometria_Computacional/Geometria_Computacional/Form1.cs
@@ -15,6 +15,15 @@ namespace Geometria_Computacional
         public Form1()
         {
             InitializeComponent();
+            Computacional_Geometry trazado = new Computacional_Geometry(pictureBox1, 640, 480);
+            int [] puntos = new int [1000];
+            trazado.DDALine(200, 200, 300, 200, puntos);
+            trazado.DDALine(200, 200, 200, 300, puntos);
+            trazado.DDALine(300, 200, 300, 300, puntos);
+            trazado.DDALine(200, 300, 300, 300, puntos);
+            trazado.BresenhamLine(200, 200, 300, 300);
+            trazado.BresenhamLine(300, 200, 200, 300);
+            trazado.setImage();
         }
     }
 }
